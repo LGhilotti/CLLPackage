eaa230c38608a1ed31935a91b3959d42c1fcf54e
lucadanese <danese.luca1@gmail.com>
2022-04-04 18:54:50 +0200

    LD: commenti che fanno funzionare

--- R/example.R
+++ R/example.R
@@ -1,24 +1,24 @@
 ## generate a dataset
-set.seed(8675309)
-n = 10000
-x1 = rnorm(n)
-x2 = rnorm(n)
-y = 1 + 0.5*x1 + 0.2*x2 + rnorm(n)
+#set.seed(8675309)
+#n = 10000
+#x1 = rnorm(n)
+#x2 = rnorm(n)
+#y = 1 + 0.5*x1 + 0.2*x2 + rnorm(n)
 
 #beta0 = rnorm(3,2,1)
 #beta0=beta_min
-beta0=c(0,0,0)
-X = cbind(rep(1,n),x1,x2)
+#beta0=c(0,0,0)
+#X = cbind(rep(1,n),x1,x2)
 
-beta_estimated_gd <- linear_gd_optim(beta0, X, y, maxit = 1000, stepsize = 0.5e-3, tol=1e-3)
+#beta_estimated_gd <- linear_gd_optim(beta0, X, y, maxit = 1000, stepsize = 0.5e-3, tol=1e-3)
 
-beta_estimated_sd <- linear_sd_optim(beta0, X, y, maxit = 1000, tol=1e-3)
+#beta_estimated_sd <- linear_sd_optim(beta0, X, y, maxit = 1000, tol=1e-3)
 
-beta_min = lm(y ~ x1 + x2)$coeff
+#beta_min = lm(y ~ x1 + x2)$coeff
 
-norm(y-X%*% beta_min )^2
-norm(y-X%*% beta_estimated_gd )^2
-norm(y-X%*% beta_estimated_sd )^2
+#norm(y-X%*% beta_min )^2
+#norm(y-X%*% beta_estimated_gd )^2
+#norm(y-X%*% beta_estimated_sd )^2
 
 
 library(tictoc)
--- R/graphics_prova.R
+++ R/graphics_prova.R
@@ -1,31 +1,31 @@
-set.seed(8675309)
-n = 1000
-x1 = rnorm(n)
-y = 1 + 0.5*x1 + rnorm(n)
+#set.seed(8675309)
+#n = 1000
+#x1 = rnorm(n)
+#y = 1 + 0.5*x1 + rnorm(n)
 
-X = cbind(rep(1,n),x1)
+#X = cbind(rep(1,n),x1)
 
-L = function(beta_0, beta_1) norm(((X %*% as.vector(c(beta_0,beta_1)) - y)),type="2")^2
+#L = function(beta_0, beta_1) norm(((X %*% as.vector(c(beta_0,beta_1)) - y)),type="2")^2
 
-L(1,0.5)
+#L(1,0.5)
 
-plot(L)
+#plot(L)
 
 ###
-library(graphics)
+#library(graphics)
 #define x and y
-beta_0 <- seq(-10,10,by=0.005)
-beta_1 <- seq(-10,10,by=0.005)
+#beta_0 <- seq(-10,10,by=0.005)
+#beta_1 <- seq(-10,10,by=0.005)
 
-beta_0 <- c(-0.5,0,0.5)
-beta_1 <- c(-0.5,0,0.5)
+#beta_0 <- c(-0.5,0,0.5)
+#beta_1 <- c(-0.5,0,0.5)
 
 #define function to create z-values
 
-prova <- cbind(beta_0,beta_1)
+#prova <- cbind(beta_0,beta_1)
 
 #create z-values
-z = mapply(L,beta_0,beta_1)
+#z = mapply(L,beta_0,beta_1)
 
-library(plotly)
-plot_ly(x=beta_0, y=beta_1, z=z, type="scatter3d", mode="markers")
+#library(plotly)
+#plot_ly(x=beta_0, y=beta_1, z=z, type="scatter3d", mode="markers")
